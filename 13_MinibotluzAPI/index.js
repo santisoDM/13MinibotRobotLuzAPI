846d0eeb1d29856f4aa9995ca39a256bbe80fede
Santiago Soto <santiagosoto@datamatrixcompany.com>
2024-07-12 18:07:28 -0400

    Update index.js

--- 13_MinibotluzAPI/index.js
+++ 13_MinibotluzAPI/index.js
@@ -1,4 +1,4 @@
-const server = require('../minibotluz_api/app')
+const server = require('../13_MinibotluzAPI/app')
 
 server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
